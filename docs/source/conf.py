2aec9348b1713f39ed00f506dd0fc0c10f113a77
Jonathan Wong superclass6 <jonathanhw.wong@gmail.com>
2025-03-14 18:00:47 +0000

    Updated table format for function reference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../angular_binning'))
+autodoc_mock_imports = ['gaussian_cl_likelihood', 'healpy', 'matplotlib', 'numpy', 'pymaster', 'scipy']
+
 
 # -- Project information
 
