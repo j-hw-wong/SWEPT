0f0f8bef943759c3b10d3c34e6ac958eb1fac6d8
Jonathan Wong <98896208+j-hw-wong@users.noreply.github.com>
2026-09-19 14:52:09 +0100

    Updated notes

--- catalogue_sim/run_flask.py
+++ catalogue_sim/run_flask.py
@@ -59,7 +59,7 @@ def main():
     pipeline directory and overwritten with the parameters from the dictionary set up in flask_config
     """
 
-    rnd_seed = random.randint(100,999)    # TUNE FOR SCIENCE GOAL (minor uncertainty aspect?)
+    rnd_seed = random.randint(100,999)    # TUNE FOR SCIENCE GOAL (minor uncertainty aspect?, should be subdominant to num.noise, to minimise)
 
     pipeline_variables_path = os.environ['PIPELINE_VARIABLES_PATH']
     config_dict = flask_config(pipeline_variables_path=pipeline_variables_path)
